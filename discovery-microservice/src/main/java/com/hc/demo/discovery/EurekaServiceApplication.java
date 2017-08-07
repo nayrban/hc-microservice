57a922e65793597544c5104d8f236871c459fba8
Bryan <bacuna@gpshiring.com>
2017-08-07 10:09:53 -0600

    refactor project name

--- discovery-microservice/src/main/java/com/hc/demo/discovery/EurekaServiceApplication.java
+++ discovery-microservice/src/main/java/com/hc/demo/discovery/EurekaServiceApplication.java
@@ -1,14 +1,22 @@
 package com.hc.demo.discovery;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication extends SpringBootServletInitializer{
-    
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServiceApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
     }
