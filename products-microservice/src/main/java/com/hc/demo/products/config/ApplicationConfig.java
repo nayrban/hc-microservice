91e1d82a4c77f7baadd4e25cac2dd7034cce988d
Bryan <bacuna@gpshiring.com>
2017-08-09 16:47:25 -0600

    balsamiq project

--- products-microservice/src/main/java/com/hc/demo/products/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hc.demo.products.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class ApplicationConfig extends Application {
-
-}
